d4be7c6c1b8d5db82f4bb0e7c5a90e890d087531
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-21 17:14:33 +0530

    Added: Welcome to Emp Wage

---
+++ src/main/java/com/empwagecomputation/App.java
@@ -0,0 +1,10 @@
+package com.empwagecomputation;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Employee Wage Computation" );
+    }
+}
