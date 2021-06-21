158479c82068b256608384112566d2748f1d5607
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-21 18:09:14 +0530

    Updated: Daily Emp Wage

--- src/main/java/com/empwagecomputation/App.java
+++ src/main/java/com/empwagecomputation/App.java
@@ -4,16 +4,20 @@ package com.empwagecomputation;
 public class App 
 {
 	public static final int IS_PRESENT = 1;
-	
+	public static final int FULL_DAY = 8;
+	public static final int WAGE_PER_HOUR = 20;
+			
     public static void main( String[] args )
     {
         System.out.println( "Welcome to Employee Wage Computation" );
-       
+        int dailywage= 0;
         double empCheck = Math.floor(Math.random() * 10 ) % 2;
         
         if (empCheck == IS_PRESENT)
         {
-        	System.out.println("Employee is Present");
+        	dailywage = FULL_DAY * WAGE_PER_HOUR;    
+         	System.out.println("Employee is Present ");
+         	System.out.println("Daily wage is :"+ dailywage);
         }
         else
         {
